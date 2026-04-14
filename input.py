2848e3c37300ef35dbf1387cd6a3a5974c02e0cf
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-14 21:38:53 +0530

    added

--- input.py
+++ input.py
@@ -7,11 +7,17 @@
 
 
 
-xo = input()            #34
-print(xo)               #34
-print(type(xo))         #<class 'str'>
+# xo = input()            #34
+# print(xo)               #34
+# print(type(xo))         #<class 'str'>
 
 
-y=int(xo)                            #converting the string type of xo into integer
-print("after type conversion")
-print(type(y))                       #now the type becomes integer 
+# y=int(xo)                            #converting the string type of xo into integer
+# print("after type conversion")
+# print(type(y))                       #now the type becomes integer 
+
+
+
+##PRINTING MULTIPLE VALUES
+# print("abc","def",45,67)              #comma is a default seperator
+# print("abc","def",45,67,sep=";")     #now everything will be spearated using ;
