3d96d317b5c8c4d11f5ea5c6230f31ebafe1de9f
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-14 16:48:37 +0530

    added

---
+++ basic.py
@@ -0,0 +1,127 @@
+# print("Hello World")
+
+# a = 5 
+# print (a)
+
+# pari_13 = 2004
+# print(pari_13)
+
+# _pari = 21
+# print(_pari)
+
+# help("keywords")
+
+# x = 5
+# print(id(x))
+# print(x)
+
+# number = 24
+# print(type(number))
+# print (number)
+
+# f = 2.33
+# print(f)
+# print(type(f))
+
+# b = True
+# print(type(b))
+# print(b)
+
+# a = "paridhi 24 @ +"
+# print(type(a))
+# print(a)
+
+# b = """this is it!"""
+# print(b)
+# print(type(b))
+
+# n = None
+# print(n)
+# print(type(n))
+
+# name = input()
+# print(name)
+# print(type(name))
+
+# x = input()
+# print(type(x))     here numers are taken as string 
+
+# x = input()
+# print(type(x))
+
+# x = input()
+# b = int(x)
+# print(type(b))
+
+# c = float ("1.5")
+# print(type(c))
+# int(c)
+
+
+# print("Yuri",21)
+# print("Jay Jay",24)
+
+# print("yuri","jay",21,24, sep=",")
+
+# print("yuri",21,sep="\n")
+
+# print("yuri",21, sep="\n" , end=" -> ")
+# print("jay",24)
+
+# a = 5 
+# b = 3
+# # # print(a + b) 
+# # # print(a - b)
+# # print(a / b)
+
+# # print(5/5)
+
+# print(a*b)
+
+# print(1.0 + 2.8)
+
+# print("1" +"1")
+
+# first = "Yuri"
+# print(first * 3)
+
+# a = 5 
+# b = 3
+# # print(a,b)
+# # print(a % b)
+# print(a // b)
+
+# print(2 ** 3)
+
+# a = 3
+# b = 4
+# print ( a == b)
+# print(a != b)
+#print(a>b)
+# print(a<b)
+# print(a <= b)
+
+# a = a+3
+# a += 3
+# a /= 2
+# print(a)
+
+# print(3 > 2)
+
+# print(4>2 and 3>2)    and opertaor
+
+# print(3>2 or 4<2)      or opertaor
+
+# print(3>2)
+# print( not 3>2)
+
+# name = "yuri jay"
+# print("Y" in name)
+# print("j" in name)
+
+# a=5
+# b=5
+# print(id(a), id(b))
+# print(a is b)
+
+print(0 or 1)
---
+++ calender.py
@@ -0,0 +1,6 @@
+import calendar
+yy = 2026 #year
+mm = 4 #month
+# display the calender
+print(calendar.month(yy,mm))
+
