838a9448716647d8bce5da7114aff1a9ca5cbea0
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-14 19:57:16 +0530

    added

--- practice.py
+++ practice.py
@@ -1,9 +1,34 @@
 #help("keywords")        #gives the list of already existing keywords
 
-x = 5
-print(id(x))   # gives the memory location of the variable 
 
-x= 23         # new value will be assigned to x 
-print(id(x))
+## VARIABLES
+# x = 5
+# print(id(x))   # gives the memory location of the variable 
 
-print(type(x))    # gives the type of x here x is of type integer 
+
+
+# x= 23         # new value will be assigned to x 
+# print(id(x))
+
+
+
+## INTEGER DATA TYPE
+# print(type(x))    # gives the type of x here x is of type integer 
+
+
+
+## BOOLEAN DATA TYPE
+# b= True
+# print(b)
+# print(type(b))
+
+
+
+##FLOAT DATA TYPE
+# f = 2.45
+# print(f)
+# print(type(f))
+
+
+
+
