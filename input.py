b37d89eef92278865564e209c7627228486b436a
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-14 20:39:46 +0530

    added

---
+++ input.py
@@ -0,0 +1,17 @@
+#to take input from user
+
+                                   #the default type of input function is string
+
+# name = input()
+# print (name)
+
+
+
+xo = input()            #34
+print(xo)               #34
+print(type(xo))         #<class 'str'>
+
+
+y=int(xo)                            #converting the string type of xo into integer
+print("after type conversion")
+print(type(y))                       #now the type becomes integer 
