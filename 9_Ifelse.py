6e08aedfeca1a7163ee3072c159d4f3208a3147b
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-16 21:07:38 +0530

    added

--- 9_Ifelse.py
+++ 9_Ifelse.py
@@ -1,6 +1,29 @@
+      ##IF-ELSE SATAEMENT
+# age = int(input())
+# if age > 18:
+#     print("can drive")
+# else:
+#     print( "cant drive")
 
-age = int(input())
-if age > 18:
-    print("can drive")
+
+
+      ##NESTED IF-ELSE CONDITION
+# age = int(input())
+# if age>18:
+#     if age >=65:
+#         print("hier a driver")
+#     else:
+#         print("you can drive")
+# else:
+#     print("you cant drive")
+
+
+
+       ##IF ELIF ELSE CONDITION
+num = int (input())
+if num < 0:
+    print("negative number")
+elif num==0:
+    print("zero")
 else:
-    print( "cant drive")
+    print("positive number")
