a64a2f6ccdf0c0eff6ad7838b8b831cebdb76408
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-17 14:31:27 +0530

    added

--- Practice Questions/7_EvenNo.py
+++ Practice Questions/7_EvenNo.py
@@ -5,3 +5,12 @@ while i<=10:
     if i%2==0:
       print(i, end = " ")
     i += 1
+    
+
+
+    ##FOR PRINTING ODD NUMBERS
+# i=1
+# while i<=10:
+#    if i%2 != 0:
+#       print(i,end=" ")
+#    i+=1
