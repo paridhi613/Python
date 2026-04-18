a4474f206676152ad18881e03431d12e3b9ab2c5
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-19 00:41:27 +0530

    added

---
+++ Practice Questions/9_Tableof5.py
@@ -0,0 +1,10 @@
+##TO PRINT TABLE OF 5
+for i in range(1,11):
+    print(5*i, end=" ")
+
+
+
+     ##TO PRINT TABLE OF ANY INTERGER OF UR CHOICE
+# n=int(input())
+# for j in range(1,11):
+#     print(j*n, end=" ")
