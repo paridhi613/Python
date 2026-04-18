3bf0fbdf23026892f5e35fb483038341f53adb82
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-19 00:01:12 +0530

    added

---
+++ 11_RangeFxn.py
@@ -0,0 +1,7 @@
+print(list(range(1,5)))        #st=1 end=5 end is not included here 
+
+print(list(range(1,10,2)))     #st=1, end=10 ,jump=2 takes a jump of 2 element
+
+print(list(range(10,1,1)))     #gives an empty array because no list is prsent at the right side of start  
+
+print(list(range(10,1,-1)))    #gives the list of reverse numbers 
