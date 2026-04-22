c3d07ca3982f4be8c913ccc122f5c313b1fc1619
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-23 00:50:40 +0530

    added

---
+++ Strings/3_indexing.py
@@ -0,0 +1,9 @@
+name = "Paridhi"
+
+print(name[4])     #returns the letter at 4th index  i.e 'd'
+
+print(name[-5])   #return the letter at the 5th index from the last i.e 'r'
+
+
+#to calculate the length of the string 
+print(len(name))       #which is 7 here
