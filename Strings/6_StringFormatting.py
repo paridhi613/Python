1c390ad7ef39379e81513bf00769b2ff48f3ef17
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-23 20:10:06 +0530

    added

---
+++ Strings/6_StringFormatting.py
@@ -0,0 +1,9 @@
+name = input()
+age = input()
+
+#traditional method of priting 
+print("hey my name is", name , "and i am", age, "years old")  
+
+
+#using formatting for the ease.
+print("Hey! My name is {}. I am {} years old. ".format(name, age))
