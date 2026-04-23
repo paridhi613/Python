a7efc6ee4573d63f055f26c4bc27d909fbfb1b35
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-23 12:44:21 +0530

    added

--- Strings/5_StringMethod.py
+++ Strings/5_StringMethod.py
@@ -5,3 +5,7 @@ print(name.capitalize())    #makes the first letter capital
 print(name.title())    #makes the first letter capital of every word
 
 print(name.upper())   #makes each letter capital of the string
+
+print(name.lower())   #makes each letter in lowercase 
+
+print(name.find("k"))   #return the lowest index of the letter
