7ba0aadad7260098a18545abab2fbd4aeb34e025
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-23 20:23:09 +0530

    added

--- Strings/6_StringFormatting.py
+++ Strings/6_StringFormatting.py
@@ -7,3 +7,7 @@ print("hey my name is", name , "and i am", age, "years old")
 
 #using formatting for the ease.
 print("Hey! My name is {}. I am {} years old. ".format(name, age))
+
+
+#another shortcut
+print(f"Hey! My name is {name}. I am {age} years old.")
