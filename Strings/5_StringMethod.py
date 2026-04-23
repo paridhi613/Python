9fa339e06d8a83a2d19444c25816362e3d9c4870
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-23 12:37:42 +0530

    added

--- Strings/5_StringMethod.py
+++ Strings/5_StringMethod.py
@@ -3,3 +3,5 @@ name = "paridhi kalyane"
 print(name.capitalize())    #makes the first letter capital
 
 print(name.title())    #makes the first letter capital of every word
+
+print(name.upper())   #makes each letter capital of the string
