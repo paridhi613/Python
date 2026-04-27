843106144f04527028e5ea1ae8e6378cb00622ca
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-27 16:50:04 +0530

    added

--- Lists/5_slicing.py
+++ Lists/5_slicing.py
@@ -4,3 +4,5 @@ print(l[0:3])  #prints the first three elements
 print(l[::])   #prints the whole list
 
 print(l[::-1])   #prints the whole list but in reverse
+
+print(l[5:0:-1]) #start from 5th index and goes till 1 index  in reverse order 
