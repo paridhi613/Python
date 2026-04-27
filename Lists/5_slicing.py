18c8ee101afa74af7418c73df9caf68e49352204
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-27 16:44:31 +0530

    added

--- Lists/5_slicing.py
+++ Lists/5_slicing.py
@@ -1,2 +1,6 @@
 l = [1,2,3,4,5,6,7]
-print(l[0:3])
+print(l[0:3])  #prints the first three elements
+
+print(l[::])   #prints the whole list
+
+print(l[::-1])   #prints the whole list but in reverse
