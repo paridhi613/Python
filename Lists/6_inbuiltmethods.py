0c46f86d6443857b3014b7364a84b2809b58e403
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-27 20:19:51 +0530

    added

--- Lists/6_inbuiltmethods.py
+++ Lists/6_inbuiltmethods.py
@@ -6,15 +6,21 @@ print(task.count(544))
 #2. INDEX - return the index of 1st occurance of the object
 print(task.index(643))
 
+
 #3. POP - remove and return the last ele of the list
 drop = task.pop()
 print(drop)
+
 
 #4. REMOVE - remove the object of our choice
 task.remove(643)
 print(task)
 
+
 #5. SORT - sorts the list in asc by default
 name =[100,345,689,908,456]
 name.sort()
 print(name)
+
+
+#6. 
