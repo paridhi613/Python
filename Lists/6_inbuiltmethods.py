d667e2248d5310bed76f7ab3e882019e5ca7df0a
paridhi613 <kalyaneparidhi94@gmail.com>
2026-04-27 19:54:25 +0530

    added

---
+++ Lists/6_inbuiltmethods.py
@@ -0,0 +1,8 @@
+#1. COUNT - give the count of the element present in the list
+task = [133, 544, 643, 6432]
+print(task.count(544))
+
+
+#2. INDEX - return the index of 1st occurance of the object
+print(task.index(643))
+
