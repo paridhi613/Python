c10ffaa7a910be829b728b68a0df9204b6a63c0a
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-16 18:58:45 +0530

    added

--- Tuples/2.py
+++ Tuples/2.py
@@ -13,3 +13,11 @@ print(t1)
 t3=(3,)               #we need to add a comma to make it a tuple, otherwise it will be considered as an integer
 print(t3)
 print(type(t3))       #<class 'tuple'>
+
+
+
+
+#we can also create a tuple using the tuple() constructor
+t3=tuple("jungkook")
+print(t3)            #('j', 'u', 'n', 'g', 'k', 'o', 'o', 'k')
+#the string is converted into a tuple of characters
