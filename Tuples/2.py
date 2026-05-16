31009f20899cc0e407a2f58069026652da8f4c38
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-16 18:55:36 +0530

    added

--- Tuples/2.py
+++ Tuples/2.py
@@ -7,6 +7,7 @@ print(t)
 print(t1)
 
 #for creating a non empty tuple
-t3=(3,)    #we need to add a comma to make it a tuple, otherwise it will be considered as an integer
+
+t3=(3,)               #we need to add a comma to make it a tuple, otherwise it will be considered as an integer
 print(t3)
-print(type(t3))   #<class 'tuple'>
+print(type(t3))       #<class 'tuple'>
