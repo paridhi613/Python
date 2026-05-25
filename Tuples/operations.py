a11abe8e657ac59ac84068fc805c27ee8de2c517
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-25 20:03:32 +0530

    added

--- Tuples/operations.py
+++ Tuples/operations.py
@@ -3,3 +3,7 @@
 t=(1,2,3,4,5)
 print(t.count(3))   #1 
 print(t.count(6))   #0   gives 0 for non existing elements
+
+
+#index: gives the index of the first occurrence of the element in the tuple
+print(t.index(5))   #4
