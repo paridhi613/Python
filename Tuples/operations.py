b2f62f58cfb019e3e404b8a882856183de1dc4f4
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-25 20:04:44 +0530

    added

--- Tuples/operations.py
+++ Tuples/operations.py
@@ -7,3 +7,6 @@ print(t.count(6))   #0   gives 0 for non existing elements
 
 #index: gives the index of the first occurrence of the element in the tuple
 print(t.index(5))   #4
+
+t1=(1,2,3,4,5,3)
+print(t1.index(3))  #2   gives the index of the first occurrence of the element in the tuple
