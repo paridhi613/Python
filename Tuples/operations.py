23962cc89c7af6206e0ce729a4637a6180dbdac7
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-25 20:16:14 +0530

    added

--- Tuples/operations.py
+++ Tuples/operations.py
@@ -10,3 +10,8 @@ print(t.index(5))   #4
 
 t1=(1,2,3,4,5,3)
 print(t1.index(3))  #2   gives the index of the first occurrence of the element in the tuple
+
+
+#iteration: we can iterate through the elements of the tuple using a for loop
+for i in t:
+    print(i**2, end=" ")   #1 4 9 16 25
