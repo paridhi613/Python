832597c5de963304f8537dea37ee94d7e73db46a
paridhi613 <kalyaneparidhi94@gmail.com>
2026-08-11 21:02:16 +0530

    added

---
+++ Dictionary/accessing.py
@@ -0,0 +1,5 @@
+#accessing data from the dictionary
+
+fruits = {'apple': 120, 'banana': 30}
+print(fruits['apple'])    #gives the value of the key 'apple'
+print(fruits['banana'])   #gives the value of the key 'banana'
