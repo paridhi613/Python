285e64467af3e44090026e0f830516838f1578a5
paridhi613 <kalyaneparidhi94@gmail.com>
2026-08-11 21:08:24 +0530

    added

--- Dictionary/accessing.py
+++ Dictionary/accessing.py
@@ -3,3 +3,9 @@
 fruits = {'apple': 120, 'banana': 30}
 print(fruits['apple'])    #gives the value of the key 'apple'
 print(fruits['banana'])   #gives the value of the key 'banana'
+
+
+
+#using get method
+print(fruits.get('apple'))    #gives the value of the key 'apple'
+print(fruits.get('orange','not in my vocabulary'))  #gives a default value if the key is not found
