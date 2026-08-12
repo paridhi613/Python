e4325a13a1f03160e260a72571b416884948f221
paridhi613 <kalyaneparidhi94@gmail.com>
2026-08-12 21:34:25 +0530

    added

--- Dictionary/updating.py
+++ Dictionary/updating.py
@@ -7,3 +7,9 @@ print(fruits)
 #updating a new value 
 fruits['guava']=48
 print(fruits)    #guava will be added to the dict
+
+
+#updating a whole new dictionary in the existing dictionary
+new={'kiwi': 20, 'mango': 30}
+fruits.update(new)
+print(fruits)    #new dictionary will be added to the existing dictionary
