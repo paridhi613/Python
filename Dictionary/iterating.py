2babf2d4f2116e32cd0940e520dcfd8e17e65a3c
paridhi613 <kalyaneparidhi94@gmail.com>
2026-08-13 19:13:13 +0530

    added

--- Dictionary/iterating.py
+++ Dictionary/iterating.py
@@ -2,3 +2,14 @@
 fruits={'apple': 34, 'banana':59, 'guava':48}
 for i in fruits:
     print(i)  #prints the keys of the dictionary
+
+print("")  #prints a new line
+
+for i in fruits:
+    print(fruits[i])  #prints the values of the dictionary
+
+
+print("")  #prints a new line
+
+for i in fruits:
+    print(i, fruits[i])  #prints the key-value pair of the dictionary
